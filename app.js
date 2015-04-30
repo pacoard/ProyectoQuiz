879494ccb141cad6798146afe7dbaa0560227979
pacoard <pacoard@gmail.com>
2015-04-30 18:02:10 +0200

    Empezando el proyecto

---
+++ app.js
@@ -0,0 +1,63 @@
+//Importar middlewares
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+//Importar enrutadores
+var routes = require('./routes/index');
+var users = require('./routes/users');
+//Crear aplicación
+var app = express();
+
+//Instalar vistas EJS
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//Instalar middlewares
+// uncomment after placing your favicon in /public
+app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Instalar enrutadores y asociar rutas a sus gestores
+app.use('/', routes);
+app.use('/users', users);
+
+// Para el resto de rutas, catch 404 y se pasa el error al error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+//---------------------Error handlers----------------------------
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+//Exportar aplicación para que se pueda arrancar desde bin/www
+module.exports = app;
