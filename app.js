108e7156ea5c90fc17e439b49d04148ceaf615c8
pacoard <pacoard@gmail.com>
2015-05-01 16:58:32 +0200

    Terminado, a la espera de una rama para la página de los créditos...

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(partials());
 
 //Instalar middlewares
 // uncomment after placing your favicon in /public
-app.use(favicon(__dirname + '/public/favicon.ico'));
+//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
