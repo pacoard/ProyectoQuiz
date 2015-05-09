a0b0c0c7630ce03a67b646b30b07074574aa49be
pacoard <pacoard@gmail.com>
2015-05-09 15:31:35 +0200

    Quiz 10 y cajetín de búsquedas hechos: unir ramas y desplegar en Heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -2,10 +2,19 @@
 var models = require('../models/models.js'); //para acceder a la BBDD,
 //ya que el módulo models.js exporta "Quiz"
 
+//Quiz 10: Autoload. Factoriza el código si la ruta incluye :quizId
+exports.load = function(req,res,next,quizId) {
+	models.Quiz.find(quizId).then( 
+		function(quiz) {
+			if (quiz) { req.quiz = quiz; next(); }
+			else next(new Error('No existe el quiz con ID='+quizId));
+		}).catch(function(error) { next(error);});
+};
+
 //Get /quizes
 exports.index = function(req,res) {
 	if (req.query.busqueda != null) {
-		var search = req.query.busqueda+'%';
+		var search = '%'+req.query.busqueda+'%'; //para buscar palabras intermedias
 		models.Quiz.findAll({where: ["pregunta like ?", search]})
 		.then(function (quizes) {
 			res.render('quizes/index', {quizes: quizes});
@@ -19,17 +28,25 @@ exports.index = function(req,res) {
 };
 //Get /quizes/:id
 exports.show = function(req,res) { //para la lista de preguntas
-	models.Quiz.find(req.params.quizId).then(function (quiz) {
-		res.render('quizes/show', {quiz: quiz});
-	})
+	//models.Quiz.find(req.params.quizId).then(function (quiz) {
+	//	res.render('quizes/show', {quiz: quiz});
+	//})
+	res.render('quizes/show', { quiz: req.quiz} );
 };
 
 //Get quizes/answer
-exports.answer = function(req,res) {
+/*exports.answer = function(req,res) {
 	models.Quiz.find(req.params.quizId).then(function (quiz) {
 		if (req.query.respuesta === quiz.respuesta)
 			res.render('quizes/answer', {respuesta: 'Correcto', quizId: req.params.quizId});
 		else 
 			res.render('quizes/answer', {respuesta: 'Incorrecto', quizId: req.params.quizId});
 	})
+};*/
+
+exports.answer = function(req,res) {
+	if (req.query.respuesta === req.quiz.respuesta)
+		res.render('quizes/answer', {respuesta: 'Correcto', quizId: req.quiz.id});
+		
+	else res.render('quizes/answer', {respuesta: 'Incorrecto', quizId: req.quiz.id});
 };
--- routes/index.js
+++ routes/index.js
@@ -9,13 +9,16 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+//Autoload: gestión de comandos con :quizId
+router.param('quizId', quizController.load);
+
 //Rutas nuevas, para la lista de preguntas
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 //CAJETÍN DE BÚSQUEDA: GET  /quizes?search=texto_a_buscar
-router.get('/quizes?busqueda=:busqueda(\\w)', quizController.index)
+router.get('/quizes?busqueda=:busqueda(\\w)', quizController.index);
 //Comentadas estas rutas para sustituirlas por la lista de preguntas
 //router.get('/quizes/question', quizController.question);
 //router.get('/quizes/answer', quizController.answer);
